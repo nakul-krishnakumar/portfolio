aa419150e978f15da60b6c1718038213fe64e4c1
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-01-18 16:33:54 +0000

    Added git-globe component

--- app/(root)/skillsPage.tsx
+++ app/(root)/skillsPage.tsx
@@ -1,16 +1,19 @@
+import { GlobeAnimation } from "@/components/GlobeAnimation";
 
 const SkillsPage = () => {
   return (
     <section id="skills" className="w-full px-3 text-center">
         <h1 className="font-bold text-[56px]">SKILLS</h1>
-        <section className="grid grid-cols-1 justify-center w-full gap-1">
-            <div className="skills-grid-item"></div>
-            <div className="skills-grid-item"></div>
-            <div className="skills-grid-item"></div>
-            <div className="skills-grid-item"></div>
-            <div className="skills-grid-item"></div>
-            <div className="skills-grid-item"></div>
-            <div className="skills-grid-item"></div>
+        <section className="grid grid-cols-1 grid-rows-5 sm:grid-cols-6 justify-center w-full h-[48rem] gap-1">
+            <div className="skills-grid-item sm:col-span-4"></div>
+            <div className="skills-grid-item sm:row-span-3 sm:col-span-2 hidden sm:block object-cover h-min">
+                <GlobeAnimation />
+            </div>
+            <div className="skills-grid-item sm:col-span-4"></div>
+            <div className="skills-grid-item sm:row-span-3"></div>
+            <div className="skills-grid-item sm:row-span-3"></div>
+            <div className="skills-grid-item sm:col-span-2"></div>
+            <div className="skills-grid-item sm:col-span-4 sm:row-span-2"></div>
         </section>
     </section>
   )
